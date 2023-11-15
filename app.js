d9cbbb4f7c5bb5badb4986782574bb5f3b1a426c
Tomisho1758 <95eltom95@gmail.com>
2023-11-15 20:13:17 -0300

    env

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT=process.env.PORT||3000
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Servidor funcionando');
 });
 
